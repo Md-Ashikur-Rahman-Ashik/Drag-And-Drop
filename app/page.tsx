ff08aa1868e5f6b3239a993e0371252edd843dd1
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2026-04-08 20:43:30 +0600

    Added counter component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
-import Button from "./components/Button";
+import Counter from "./components/Counter";
 import Header from "./components/Header";
-import Card from "./components/Card";
 
 export default function Home() {
   return (
     <div>
       <Header siteName="My Website Builder" />
-      <Card title="Drag And Drop" description="Build pages visually" />
-      <Card title="Templates" description="Start from a template" />
-      <Button label="Get Started" />
+      <Counter />
     </div>
   );
 }
