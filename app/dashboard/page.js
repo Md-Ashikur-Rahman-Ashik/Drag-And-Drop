f9d97086fc932a8ed339da181a8164aef289f265
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2026-04-11 15:06:37 +0000

    Updated Dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,8 +1,11 @@
+import PageManager from "../components/PageManager";
+
 export default function DashboardPage() {
   return (
     <div>
       <h1 className="text-2xl font-bold text-gray-900">Welcome Back</h1>
       <p className="text-gray-600 mt-2">Here are your sites.</p>
+      <PageManager />
     </div>
   );
 }
