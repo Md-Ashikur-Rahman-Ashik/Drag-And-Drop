909f792c017576dd825fe3d7ff3ceef2a5590330
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2026-04-11 13:19:12 +0000

    Added about page

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold">About</h1>
+      <p className="text-gray-600 mt-4">This is the about page</p>
+    </div>
+  );
+}
