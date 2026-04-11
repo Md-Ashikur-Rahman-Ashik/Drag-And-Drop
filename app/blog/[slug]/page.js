e9097969c97c1ac329b65fd900df8330913c8b11
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2026-04-11 13:31:18 +0000

    Modified blog content page

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,7 +1,8 @@
-export default function BlogPost({ params }) {
+export default async function BlogPost({ params }) {
+  const { slug } = await params;
   return (
     <div className="p-8">
-      <h1 className="text-3xl font-bold">Post : {params.slug}</h1>
+      <h1 className="text-3xl font-bold">Post : {slug}</h1>
       <p className="text-gray-600 mt-4">This page was generated from the URL</p>
     </div>
   );
