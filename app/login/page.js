e7bb119e8af7b618d3506937fbc2ebe167563d78
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2026-04-14 04:20:19 +0000

    Adding Login page

---
+++ app/login/page.js
@@ -0,0 +1,28 @@
+"use client";
+
+import { useState } from "react";
+import { createSupabaseBrowser } from "../../lib/supabase-browser";
+import { useRouter } from "next/navigation";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [isSignUp, setIsSignUp] = useState(false);
+  const router = useRouter();
+  const supabase = createSupabaseBrowser();
+
+  const handleAuth = async () => {
+    setLoading(true);
+    setError("");
+
+    const { error } = isSignUp
+      ? await supabase.auth.signUp({ email, password })
+      : await supabase.auth.signInWithPassword({ email, password });
+
+      if(error){
+        
+      }
+  };
+}
