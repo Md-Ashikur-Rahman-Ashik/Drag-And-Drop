7c93feff66e679dd16557da4d8892ab929a48875
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2026-04-15 00:30:57 +0000

    Added Sign out route

---
+++ app/api/auth/signout/route.js
@@ -0,0 +1,13 @@
+import { createSupabaseServer } from "@/lib/supabase-server";
+import { NextResponse } from "next/server";
+
+export async function POST() {
+  const supabase = await createSupabaseServer();
+  await supabase.auth.signOut();
+  return NextResponse.redirect(
+    new URL(
+      "/login",
+      process.env.NEXT_PUBLIC_SITE_URL || "https://localhost:3000",
+    ),
+  );
+}
