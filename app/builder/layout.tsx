23ff6a9a3a10e001a7bffa92819e4dba43970119
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2026-04-15 22:22:28 +0000

    Added type of children in Builder layout

--- app/builder/layout.tsx
+++ app/builder/layout.tsx
@@ -1,6 +1,11 @@
+import React from "react";
 import Navbar from "../components/Navbar";
 
-export default function BuilderLayout({ children }) {
+interface BuilderProps {
+  children: React.ReactElement;
+}
+
+export default function BuilderLayout({ children }: BuilderProps) {
   return (
     <div className="flex flex-col h-screen bg-gray-950 overflow-hidden">
       <Navbar siteName="My First Site" />
