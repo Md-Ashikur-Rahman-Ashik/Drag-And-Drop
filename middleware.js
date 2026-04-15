e7d47341e09e6e731fafe008245dfd1634126950
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2026-04-15 00:03:11 +0000

    Added middleware

--- middleware.js
+++ middleware.js
@@ -1,11 +1,41 @@
-import { createServerClient } from "@supabase/ssr"
-import { NextResponse } from "next/server"
+import { createServerClient } from "@supabase/ssr";
+import { NextResponse } from "next/server";
 
-export async function middleware(request){
-    const response = NextResponse.next()
+export async function middleware(request) {
+  const response = NextResponse.next();
 
-    const supabase = createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL,
-        
-    )
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            response.cookies.set(name, value, options),
+          );
+        },
+      },
+    },
+  );
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user && request.nextUrl.pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  if (user && request.nextUrl.pathname === "/login") {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  return response;
 }
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/builder/:path*", "/login"],
+};
