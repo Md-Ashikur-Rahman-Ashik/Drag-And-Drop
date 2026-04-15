6fd98732a37640eb133cda73643da873c1b327a2
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2026-04-15 22:17:56 +0000

    Added type of params in Blog page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,4 +1,8 @@
-export default async function BlogPost({ params }) {
+interface BlogPostProps {
+  params: Promise<{ slug: string }>;
+}
+
+export default async function BlogPost({ params }: BlogPostProps) {
   const { slug } = await params;
   return (
     <div className="p-8">
