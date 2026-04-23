e8c4a304f27f4d92afdf9cfdcc6513d34c965adf
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2026-04-23 02:42:10 +0000

    Modified Builder layout

--- app/builder/layout.tsx
+++ app/builder/layout.tsx
@@ -1,15 +1,7 @@
 import React from "react";
-import Navbar from "../components/Navbar";
 
-interface BuilderProps {
-  children: React.ReactElement;
-}
-
-export default function BuilderLayout({ children }: BuilderProps) {
+export default function BuilderLayout({ children }: {children : React.ReactNode}) {
   return (
-    <div className="flex flex-col h-screen bg-gray-950 overflow-hidden">
-      <Navbar siteName="My First Site" />
-      <div className="flex flex-1 overflow-hidden">{children}</div>
-    </div>
+    <>{children}</>
   );
 }
