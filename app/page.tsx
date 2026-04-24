e864705718c49309874e4b696d3b1ed949089727
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2026-04-24 05:31:23 +0000

    Modified button of home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-8 items-center justify-center h-screen">
       <h1 className="text-8xl font-mono">Drag And Drop Builder</h1> 
-      <Link href={"/dashboard"} className="text-4xl font-medium rounded-xl transition-all duration-150 text-gray-600 hover:bg-gray-100 hover:text-gray-900 px-6 py-3">
+      <Link href={"/dashboard"} className="text-4xl font-medium rounded-xl transition-all duration-150 bg-black text-white hover:bg-gray-700 px-6 py-3">
         Navigate to Dashboard
       </Link>
     </div>
