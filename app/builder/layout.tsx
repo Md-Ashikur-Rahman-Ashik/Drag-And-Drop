ea9b9b71b406498aacbc6c2b437f2cfc7e5d850d
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2026-04-24 05:38:38 +0000

    Removed custom components from builder layout

--- app/builder/layout.tsx
+++ app/builder/layout.tsx
@@ -1,15 +1,9 @@
 import React from "react";
-import Navbar from "../components/Navbar";
 
 export default function BuilderLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div className="flex flex-col h-screen bg-[#111] overflow-hidden">
-      <Navbar />
-      <div className="flex flex-1 overflow-hidden">{children}</div>
-    </div>
-  );
+  return <div className="h-screen overflow-hidden">{children}</div>;
 }
