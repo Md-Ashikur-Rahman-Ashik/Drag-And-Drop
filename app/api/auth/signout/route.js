fbd82d49879eacddd5af32a2b92fafb78b9c3b52
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2026-04-25 02:44:05 +0000

    Fixed supabase server route

--- app/api/auth/signout/route.js
+++ app/api/auth/signout/route.js
@@ -1,11 +1,11 @@
-import { createSupabaseServer } from "@/lib/supabase-server"
+import { createSupabaseServer } from "../../../lib/supabase-server"
 import { NextResponse } from "next/server"
 
 export async function POST() {
   const supabase = await createSupabaseServer()
   await supabase.auth.signOut()
 
-  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL  
+  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL
 
   return NextResponse.redirect(new URL("/login", siteUrl))
 }
