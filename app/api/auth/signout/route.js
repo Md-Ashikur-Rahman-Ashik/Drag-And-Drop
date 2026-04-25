aaa7d4aaf06648849cba28acf4622ca871ffcb5b
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2026-04-25 02:41:26 +0000

    Removed error from signout route's code

--- app/api/auth/signout/route.js
+++ app/api/auth/signout/route.js
@@ -1,13 +1,11 @@
-import { createSupabaseServer } from "../../../lib/supabase-server";
-import { NextResponse } from "next/server";
+import { createSupabaseServer } from "@/lib/supabase-server"
+import { NextResponse } from "next/server"
 
 export async function POST() {
-  const supabase = await createSupabaseServer();
-  await supabase.auth.signOut();
-  return NextResponse.redirect(
-    new URL(
-      "/login",
-      process.env.NEXT_PUBLIC_SITE_URL || "https://localhost:3000",
-    ),
-  );
+  const supabase = await createSupabaseServer()
+  await supabase.auth.signOut()
+
+  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL  
+
+  return NextResponse.redirect(new URL("/login", siteUrl))
 }
